d1e9113e07f17eeecb8c3a2ed68ce1ac1f595ec2
Thanapa <eve.jitthana@gmail.com>
2023-04-24 10:57:29 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class program{
+    static void Main(string[] args){
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("--------detail---------");
+        Console.WriteLine("-----------------------");
+        
+        Console.Write("Pls input amount of item : ");
+        int amount = int.Parse(Console.ReadLine());
+        int x = 1;
+        
+        while(x >= 0 || x <= amount){
+                Console.Write("Pls input name of item : ");
+                string name = Console.ReadLine();
+                Console.Write("Pls input type of item : ");
+                string type = Console.ReadLine();
+                Console.WriteLine("");
+                x++;
+        }
+                Console.Write("Pls input Mode : ");
+                string mode = Console.ReadLine();
+
+                if(mode == "ShowAll"){
+                    Console.WriteLine(name);
+                    Console.WriteLine(type);    
+                } else if(mode == type){
+                    Console.WriteLine(name);
+                } else {
+                    Console.Write("End");
+            
+            }
+            
+    }
+    
+}
